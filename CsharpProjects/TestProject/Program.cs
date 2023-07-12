af179aa91d459a7dbc1f8f4e0494ab727a0225ff
kmh824 <113575494+kmh824@users.noreply.github.com>
2023-07-12 14:27:48 +0900

    test

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,13 @@
+﻿// Random dice = new Random();
+// int roll = dice.Next(1, 7);
+// Console.WriteLine(roll);
+
+Random dice = new Random();
+
+int Roll1 = dice.Next();
+int Roll2 = dice.Next(11);
+int Roll3 = dice.Next(1, 11);
+
+Console.WriteLine($"first number is {Roll1}");
+Console.WriteLine($"second number is {Roll2}");
+Console.WriteLine($"third number is {Roll3}");
