3ab4c1e26c2496e6727ec8c89f2fe937408a8345
kmh824 <1rkdalsgur1@naver.com>
2023-07-13 11:02:43 +0900

    commit message

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,6 +1,42 @@
-﻿// Random dice = new Random();
-// int roll = dice.Next(1, 7);
-// Console.WriteLine(roll);
+﻿//리터럴 및 변수 값을 사용하여 데이터 저장 및 검색
+/*
+var Name = "Bob";
+var Integer = 3;
+var Float = 34.4;
+
+Console.WriteLine($"Hello, {Name}! You have {Integer} in your inbox. The temperature is {Float} celsius");
+ */
+
+//문자 이스케이프 시퀀스를 사용하여 출력 형식 지정
+/*
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+
+//축자 문자열 리터럴
+Console.Write(@"c:\invoices");
+
+//유니코드 이스케이프 문자를 사용하여 출력 형식 지정
+// To generate Japanese invoices:
+// Nihon no seikyū-sho o seisei suru ni wa:
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n");
+// User command to run an application
+Console.Write(@"c:\invoices\app.exe -j");
+*/
+
+//리터럴 문자열과 변수 연결
+String greeting = "hello";
+String firstName = "Bob";
+Console.WriteLine(greeting + " " + firstName);
+
+
+
+
+/*
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
 
 Random dice = new Random();
 
@@ -11,3 +47,4 @@ int Roll3 = dice.Next(1, 11);
 Console.WriteLine($"first number is {Roll1}");
 Console.WriteLine($"second number is {Roll2}");
 Console.WriteLine($"third number is {Roll3}");
+*/
