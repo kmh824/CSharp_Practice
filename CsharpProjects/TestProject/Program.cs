0864904e29b10c36e7075c1e62204599d1edee17
kmh824 <1rkdalsgur1@naver.com>
2023-07-14 13:53:57 +0900

    commit message

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -69,6 +69,8 @@ largerValue = Math.Max(firstValue, secondValue);
 Console.WriteLine(largerValue);
 */
 
+//주사위 게임
+/*
 Random dice = new Random();
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
@@ -77,4 +79,18 @@ int roll3 = dice.Next(1, 7);
 int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)){
+    total += 2;
+    Console.WriteLine(@$"You rolled doubles! +2 bonus to total!
+current score: {total}");
+}
+else if ((roll1 == roll2) && (roll2 == roll3)){
+    total += 6;
+    Console.WriteLine(@$"You rolled triples! +6 bonus to total!
+current score: {total}");
+}
+if (total >= 15) Console.WriteLine("You win!");
+else Console.WriteLine("Sorry, You lose.");
+*/
 
