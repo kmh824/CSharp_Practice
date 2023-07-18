2f95f8b200b2bcf7868dfd66f7707935c94bf342
kmh824 <1rkdalsgur1@naver.com>
2023-07-18 13:14:15 +0900

    commit message

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -184,7 +184,7 @@ Console.WriteLine(1 < 2);
 Console.WriteLine(1 >= 1);
 Console.WriteLine(1 <= 1);
 */
-
+/*
 string pangram = "The quick brown fox jumps over the lazy dog.";
 Console.WriteLine(pangram.Contains("fox"));
 Console.WriteLine(!pangram.Contains("cow"));
@@ -193,4 +193,19 @@ int saleAmount = 1001;
 Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
 
 Random coin = new Random();
-Console.WriteLine(coin.Next(0, 2) == 0 ? "heard" : "tails");
+Console.WriteLine((coin.Next(0, 2) == 0) ? "heard" : "tails");
+*/
+string permission = "Admin|Manager";
+int level = 56 ;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55) Console.WriteLine("Welcome, Super Admin user.");
+    else Console.WriteLine("Welcome, Admin user.");
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20) Console.WriteLine("Contact an Admin for access.");
+    else Console.WriteLine("You do not have sufficient privileges.");
+}
+else Console.WriteLine("You do not have sufficient privileges.");
