d049defdbc3418947f514e60be434104f7f03dd5
kmh824 <1rkdalsgur1@naver.com>
2023-07-21 13:20:53 +0900

    commit message

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -353,9 +353,10 @@ string? readResult;
 string valueEntered = "";
 int numValue = 0;
 bool validNumber = false;
+
+Console.WriteLine("Enter an integer value between 5 and 10");
 do
 {
-    Console.WriteLine("Enter an integer value between 5 and 10");
     readResult = Console.ReadLine();
 
     if (readResult != null)
@@ -380,17 +381,29 @@ do
 Console.WriteLine($"Your input value ({numValue}) has been accepted.");
 */
 
-string permission = "administrator|aanager|user";
 string? readResult;
 string roleName = "";
 bool validEntry = false;
+
 do
-{
+{                
     Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
     readResult = Console.ReadLine();
-
-    if (readResult != null){
-        roleName = readResult;
+    if (readResult != null) 
+    {
+        roleName = readResult.Trim();
     }
-    
-} while (true);
+
+    if (roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user") 
+    {
+        validEntry = true;
+    }
+    else
+    {
+        Console.Write($"The role name that you entered, \"{roleName}\" is not valid. ");
+    }
+
+} while (validEntry == false);
+
+Console.WriteLine($"Your input value ({roleName}) has been accepted.");
+readResult = Console.ReadLine();
