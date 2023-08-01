4a7e17b5512a91817e6703cbcdf99905c07f7f62
kmh824 <1rkdalsgur1@naver.com>
2023-08-01 22:10:18 +0900

    commit message

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -499,3 +499,22 @@ Console.WriteLine($"{total}\n{message}");
 */
 
 // 수학 연산을 특정 숫자 형식으로 출력하는 과제
+/*
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just truncate)
+decimal result1 = Convert.ToInt32(value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+// Your code here to set result2
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// Your code here to set result3
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+*/
+
