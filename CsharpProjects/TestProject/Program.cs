45ae2ffc775e20a55623f4306571a49718c7b815
kmh824 <1rkdalsgur1@naver.com>
2023-08-22 17:13:29 +0900

    commit message

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1064,25 +1064,113 @@ void emailAdress(string[,] domain)
 */
 
 // 총 구매 가격 계산
+/*
 double total = 0;
 double minimumSpend = 30.00;
 
 double[] items = {15.97, 3.50, 12.25, 22.99, 10.98};
 double[] discounts = {0.30, 0.00, 0.10, 0.20, 0.50};
 
-Console.WriteLine($"Total: ${total}");
-
-void GetDiscountedPrice(int itemIndex)
+for (int i = 0; i < items.Length; i++)
 {
-    // Calculate the discounted price of the item
+    total += GetDiscountedPrice(i);
 }
 
-void TotalMeetsMinimum()
+total -= TotalMeetsMinimum() ? 5.00 : 0.00;
+
+Console.WriteLine($"Total: ${FormatDecimal(total)}");
+
+double GetDiscountedPrice(int itemIndex)
 {
-    // Check if the total meets the minimum
+    return items[itemIndex] * (1 - discounts[itemIndex]);
 }
 
-void FormatDecimal(double input)
+bool TotalMeetsMinimum()
 {
-    // Format the double so only 2 decimal places are displayed
+    return total >= minimumSpend;
 }
+
+string FormatDecimal(double input)
+{
+    return input.ToString().Substring(0, 5);
+}
+*/
+
+// 가격과 일치하는 코인 조합 구하기
+/*
+int target = 60;
+int[] coins = new int[] {5, 5, 50, 25, 25, 10, 5};
+int[] result = TwoCoins(coins, target);
+
+if (result.Length == 0) 
+{
+    Console.WriteLine("No two coins make change");
+} 
+else 
+{
+    Console.WriteLine($"Change found at positions {result[0]} and {result[1]}");
+}
+
+int[] TwoCoins(int[] coins, int target) 
+{
+    for (int curr = 0; curr < coins.Length; curr++) 
+{
+    for (int next = curr + 1; next < coins.Length; next++) 
+    {
+        if (coins[curr] + coins[next] == target) 
+        {
+            return new int[]{curr, next};
+        }
+
+    }
+}
+    return  new int[0];
+}
+*/
+/*
+int target = 30;
+int[] coins = new int[] {5, 5, 50, 25, 25, 10, 5};
+int[,] result = TwoCoins(coins, target);
+
+if (result.Length == 0) 
+{
+    Console.WriteLine("No two coins make change");
+} 
+else 
+{
+    Console.WriteLine("Change found at positions:");
+    for (int i = 0; i < result.GetLength(0); i++) 
+    {
+        if (result[i,0] == -1) 
+        {
+            break;
+        }
+        Console.WriteLine($"{result[i,0]},{result[i,1]}");
+    }
+}
+
+int[,] TwoCoins(int[] coins, int target) 
+{
+    int[,] result = {{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
+    int count = 0;
+
+    for (int curr = 0; curr < coins.Length; curr++) 
+    {
+        for (int next = curr + 1; next < coins.Length; next++) 
+        {
+            if (coins[curr] + coins[next] == target) 
+            {
+                result[count, 0] = curr;
+                result[count, 1] = next;
+                count++;
+            }
+            if (count == result.GetLength(0)) 
+            {
+                return result;
+            }
+        }
+    }
+    return (count == 0) ? new int[0,0] : result;
+}
+*/
+// 주사위 미니 게임 과제
