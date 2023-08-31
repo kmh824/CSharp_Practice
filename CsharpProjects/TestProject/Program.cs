312b384ad8d53e624c75419eecc9bdc3e7ae4331
kmh824 <1rkdalsgur1@naver.com>
2023-08-31 19:50:50 +0900

    commit message

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1284,33 +1284,3 @@ void PrintGroup(string[,] groups)
 }
 */
 
-
-
-string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
-
-string messageText = "";
-
-foreach (string name in names)
-{
-    if (name == "Sophia")
-        messageText = SophiaMessage();
-    else if (name == "Andrew")
-        messageText = AndrewMessage();
-    else if (name == "AllGreetings")
-        messageText = SophiaMessage() + "\n\r" + AndrewMessage();
-
-    Console.WriteLine(messageText + "\n\r");
-}
-
-bool pauseCode = true;
-while (pauseCode == true);
-
-static string SophiaMessage()
-{
-    return "Hello, my name is Sophia.";
-}
-
-static string AndrewMessage()
-{
-    return "Hi, my name is Andrew. Good to meet you.";
-}
